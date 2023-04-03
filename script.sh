a8377e0636b850f8a409224a5747c8b220dc7be8
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-04-03 21:22:48 +0200

    Update script to download the main branch of the repository IUTSAES3-Website

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ git clone https://github.com/LennyGonzales/SAES3-Server.git
 
 # Téléchargement du site web
 cd site
-git clone -b mvc https://github.com/LennyGonzales/IUTAIX-SAES3-WebSite.git
+git clone https://github.com/LennyGonzales/IUTAIX-SAES3-WebSite.git
